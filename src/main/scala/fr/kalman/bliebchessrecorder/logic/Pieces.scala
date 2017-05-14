bee2fc4d5e642e187d46d3acb5dc3a6d8211fb98
David <davkalman@gmail.com>
2017-05-14 23:09:25 +0200

    Convert trait Piece to abstract class

--- src/main/scala/fr/kalman/bliebchessrecorder/logic/Pieces.scala
+++ src/main/scala/fr/kalman/bliebchessrecorder/logic/Pieces.scala
@@ -1,12 +1,24 @@
 package fr.kalman.bliebchessrecorder.logic
 
-import fr.kalman.bliebchessrecorder.logic.Color.Color
+import fr.kalman.bliebchessrecorder.logic.Color._
+import scala.collection.immutable.SortedSet
 
-sealed trait Piece { val color: Color }
+sealed abstract class Piece {
+  private val authorizedId = SortedSet('p','r','n', 'b', 'q', 'k')
+  require(authorizedId contains id)
 
-case class Pawn(color: Color) extends Piece
-case class Rook( color: Color) extends Piece
-case class Knight(color: Color) extends Piece
-case class Bishop(color: Color) extends Piece
-case class Queen(color: Color) extends Piece
-case class King(color: Color) extends Piece
+  // Implement id as lazy or the requirement above will fail, because it is not set yet (=0)
+  val id: Char
+  val color: Color
+
+  override val toString: String = if (color == WHITE) id.toUpper.toString else id.toString
+  override val hashCode: Int = (1 + authorizedId.toVector.indexOf(id)) * (if (color==BLACK) 2 else 1)
+}
+
+case class Pawn(color: Color) extends Piece { lazy val id='p' }
+case class Rook(color: Color) extends Piece { lazy val id='r' }
+case class Knight(color: Color) extends Piece { lazy val id='n' }
+case class Bishop(color: Color) extends Piece { lazy val id='b' }
+case class Queen(color: Color) extends Piece { lazy val id='q' }
+case class King(color: Color) extends Piece { lazy val id='k' }
+
---
+++ src/test/scala/PiecesSpec.scala
@@ -0,0 +1,54 @@
+import fr.kalman.bliebchessrecorder.logic._
+import fr.kalman.bliebchessrecorder.logic.Color._
+import fr.kalman.bliebchessrecorder.logic.Piece
+
+class PiecesSpec extends org.scalatest.FreeSpec {
+
+  "2 Pieces are equals if they are same kind and color" in {
+    val allPieces = List(
+      Pawn(WHITE), Rook(WHITE), Knight(WHITE), Bishop(WHITE), Queen(WHITE), King(WHITE),
+      Pawn(BLACK), Rook(BLACK), Knight(BLACK), Bishop(BLACK), Queen(BLACK), King(BLACK))
+
+    var i = 0
+    allPieces.map(p1 => allPieces.map(p2 => {
+      i = i + 1
+      assert(
+        (p1, p2) match {
+          // Pieces of different color cannot be equal
+          case (p1: Piece, p2: Piece) if p1.color != p2.color => p1 != p2
+          // Remaining matches for pieces of same color
+          case (Pawn(_), Pawn(_)) => p1 == p2
+          case (Rook(_), Rook(_)) => p1 == p2
+          case (Knight(_), Knight(_)) => p1 == p2
+          case (Bishop(_), Bishop(_)) => p1 == p2
+          case (Queen(_), Queen(_)) => p1 == p2
+          case (King(_), King(_)) => p1 == p2
+          case _ => p1 != p2
+        })
+    }))
+    info(i.toString)
+  }
+
+  "Each piece is identified by a letter" - {
+    assert(Rook(WHITE).toString === "R")
+    assert(Knight(BLACK).toString === "n")
+    assert(Bishop(WHITE).toString === "B")
+    assert(Queen(BLACK).toString === "q")
+    assert(King(WHITE).toString === "K")
+  }
+
+  "Piece hashcode" - {
+    "goes from 1 for the white bishop" in {
+      assert(Bishop(WHITE).hashCode === 1)
+    }
+    "to 6 for the white rook" in {
+      assert(Rook(WHITE).hashCode === 6)
+    }
+    "is doubled for a black piece" in {
+      assert(King(BLACK).hashCode === 2*King(WHITE).hashCode)
+    }
+    "so that the maximum possible is" in {
+      assert(Rook(BLACK).hashCode === 12)
+    }
+  }
+}
